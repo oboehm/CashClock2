7b189afad77a313daa8ce20e9a1bb4bb4c9fcc01
Oli B <boehm@javatux.de>
2015-05-05 22:52:26 +0200

    Apple watch kit added as target

---
+++ CashClock2 WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  CashClock2 WatchKit Extension
+//
+//  Created by oliver on 05.05.15.
+//  Copyright (c) 2015 Oliver Böhm. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awakeWithContext(context: AnyObject?) {
+        super.awakeWithContext(context)
+        
+        // Configure interface objects here.
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
