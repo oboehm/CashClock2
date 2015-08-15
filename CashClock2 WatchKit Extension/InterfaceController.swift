ee07a7ce4ec3556f91a0c1703abc1ede8adfb3e1
Oli B. <boehm@javatux.de>
2015-08-15 23:24:39 +0200

    timer and button added to watchkit app

--- CashClock2 WatchKit Extension/InterfaceController.swift
+++ CashClock2 WatchKit Extension/InterfaceController.swift
@@ -12,20 +12,36 @@ import Foundation
 
 class InterfaceController: WKInterfaceController {
 
+    @IBOutlet weak var moneyLabel: WKInterfaceLabel!
+    @IBOutlet weak var watchTimer: WKInterfaceTimer!
+
+    /**
+     * This method is called if start / stop button will be pressed on the
+     * watch.
+     */
+    @IBAction func buttonTapped() {
+        println("InterfaceController.\(__FUNCTION__): button was tapped.")
+        watchTimer.setDate(NSDate(timeIntervalSinceNow: 0))
+        watchTimer.start()
+    }
+    
     override func awakeWithContext(context: AnyObject?) {
         super.awakeWithContext(context)
         
         // Configure interface objects here.
+        println("InterfaceController.\(__FUNCTION__): \(context) was initialized.")
     }
 
     override func willActivate() {
         // This method is called when watch view controller is about to be visible to user
         super.willActivate()
+        println("InterfaceController.\(__FUNCTION__): controller is visible.")
     }
 
     override func didDeactivate() {
         // This method is called when watch view controller is no longer visible
         super.didDeactivate()
+        println("InterfaceController.\(__FUNCTION__): controller is no longer visible.")
     }
 
 }
